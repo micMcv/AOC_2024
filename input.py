3704cbf4c3ad42d78897d85bdd3accaef5f86b59
micMcv <>
2024-12-02 23:23:41 +0100

    modified method signature

--- input.py
+++ input.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-def load_input_from_aoc_website(day):
-    url = f"https://adventofcode.com/2024/day/{day}/input"
+def load_input_from_aoc_website(year, day):
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
     cookie = os.environ.get("AOC_SESSION")
     return requests.get(url, cookies = {"session": cookie}
     ).text
