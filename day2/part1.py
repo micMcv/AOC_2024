c754458a5c8644205f01a510c9b8940269117d89
micMcv <>
2024-12-02 23:22:50 +0100

    added solution for day 2

--- day2/part1.py
+++ day2/part1.py
@@ -1,4 +1,36 @@
 from input import load_input_from_aoc_website
-left_numbers = []
-right_numbers = []
-data = load_input_from_aoc_website(day = 1).split()
+
+result = 0
+data = load_input_from_aoc_website(year = 2024 , day = 2).splitlines()
+
+def distance_greater_three(a : int,b : int):
+    return abs(a-b) > 3
+
+def is_safe (line_ ):
+    check = True
+
+    line_ = list(map(int, line_.split(" ")))
+
+    first = line_[0]
+    second = line_[1]
+    if second == first or distance_greater_three(second, first):
+        return False
+    if second > first:
+
+        for n in range(2, len(line_)):
+            if line_[n] <= line_[n - 1] or distance_greater_three(line_[n], line_[n - 1]):
+                return False
+
+    else:
+        for n in range(2, len(line_)):
+            if line_[n] >= line_[n - 1] or distance_greater_three(line_[n - 1], line_[n]):
+                return False
+
+    return check
+
+for line in data:
+
+    if is_safe(line):
+        result += 1
+
+print(result)
--- day2/part2.py
+++ day2/part2.py
@@ -1,4 +1,47 @@
 from input import load_input_from_aoc_website
-left_numbers = []
-right_numbers = []
-data = load_input_from_aoc_website(day = 1).split()
+
+result = 0
+data = load_input_from_aoc_website(year=2024 ,day = 2).splitlines()
+
+def distance_greater_three(a : int,b : int):
+    return abs(a-b) > 3
+
+def is_safe (line_ ):
+    check = True
+    first = line_[0]
+    second = line_[1]
+    if second == first or distance_greater_three(second, first):
+        return False
+
+    if second > first:
+
+        for n in range(2, len(line_)):
+            if line_[n] <= line_[n - 1] or distance_greater_three(line_[n], line_[n - 1]):
+                return False
+
+    if second < first:
+
+        for n in range(2, len(line_)):
+            if line_[n] >= line_[n - 1] or distance_greater_three(line_[n - 1], line_[n]):
+                return False
+
+    return check
+
+for line in data:
+
+    line = list(map(int, line.split(" ")))
+
+    if is_safe(line):
+        result += 1
+
+    else:
+        for index in range(len(line)):
+            line_copy = line.copy()
+            line_copy.pop(index)
+            if is_safe(line_copy):
+                result += 1
+                break
+
+print(result)
+
+
