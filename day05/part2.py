17658e7ca3736f57dfeabb7bee9f71769cd38c4f
micMcv <>
2024-12-04 16:32:22 +0100

    cleaned up und prepared day 5

---
+++ day05/part2.py
@@ -0,0 +1,10 @@
+from input import load_input_from_aoc_website
+import helpers
+
+data = load_input_from_aoc_website(year = 2024 , day = 5).splitlines()
+result = 0
+
+print(data)
+
+
+print(result)
